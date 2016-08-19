d9a0fff88d104a6299323132e55d2dd134b528fc
kuaitoukid <363724414@qq.com>
2016-08-19 21:31:42 +0800

    cannot bear the previous code
    cannot bear the previous code

--- generate.m
+++ generate.m
@@ -1,24 +1,12 @@
-clear all
-clc
-datalist={'7-';'8-';'91-';'92-';'93-';'94-'};
-%numlist=[30000,30000,30000,30000,30000,10000];
-numlist=[10,10,10,20,10,5];
-for i=1:2
-    datanum=datalist(i);
-    datanum=char(datanum);
-    imnum=numlist(i);
-    convert_im2uint8(datanum,imnum);
-    regular_imageformat(datanum,imnum,1);
-    extract_origin_boundingbox(datanum,imnum);
-    adjust_boundingbox(datanum,imnum);
-    distort_formulas(datanum,imnum);
-end
-for i=3:6
-    datanum=datalist(i);
-    datanum=char(datanum);
-    imnum=numlist(i);
-    regular_imageformat(datanum,imnum,0);
-    extract_origin_boundingbox(datanum,imnum);
-    adjust_boundingbox(datanum,imnum);
-    distort_formulas(datanum,imnum);
+clear; clc
+datalist = {'1-', '2-', '3-', '4-', '5-', '61-', '62-', '7-', '8-', '91-', '92-', '93-', '94-', '95-'};
+% numlist = [10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000];
+for i = 1 : length(datalist)
+    datanum = datalist{i};
+    imnum = 10000;
+    % convert_im2uint8(datanum, imnum);
+    % regular_imageformat(datanum, imnum, 1);
+    % extract_origin_boundingbox(datanum, imnum);
+    % adjust_boundingbox(datanum, imnum);
+    projective_formulas(datanum, imnum); % distort_formulas(datanum, imnum);
 end
